789fa02f93124105f9e5cb946d6a9573744582d7
robo <robolab1902@naver.com>
2022-10-18 08:41:45 +0900

    목표될 단어와 제외할 단어 List 추가

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Net.Sockets;
-using System.Text;
+﻿using System.Text;
 
-args = new string[1];
+args = new string[3];
 args[0] = "D:\\GItSource\\FindWord";
+args[1] = "drop";
+args[2] = "main";
 
 //string Dir = Environment.GetCommandLineArgs()[1];
 string Dir = args[0];
@@ -11,33 +12,44 @@ DirectoryInfo directory = new DirectoryInfo(Dir);
 FileInfo[] files = directory.GetFiles();
 int[] result = new int[files.Length];
 
-for (int i = 0; i < files.Length; i++)
+List<string> GoalWords = new();
+List<string> ExceptWords = new();
+
+if (args.Length > 1)
 {
-    int lineCount = 0;
-
-    foreach (string line in File.ReadLines(files[i].ToString()))
+    for (int i = 1; i < args.Length; i++)
     {
-        string Line = line.ToUpper();
-
-        int wordPos = Line.IndexOf("DROP", 0);
-        int droppingPos = Line.IndexOf("DROPPING", 0);
-
-        lineCount++;
-
-        while (wordPos != -1)
-        {
-            if (wordPos != droppingPos)
-            {
-                Console.WriteLine("{0} : {1}줄 {2}번째 문자에 drop이 있습니다.", files[i].Name, lineCount, wordPos + 1);
-                result[i]++;
-            }
-
-            int nextWordPos = wordPos + 1;
-
-            wordPos = Line.IndexOf("DROP", nextWordPos);
-            droppingPos = Line.IndexOf("DROPPING", nextWordPos);
-        }
+        GoalWords.Add(args[i]);
     }
 
-    Console.WriteLine("{0} 에는 drop 이 {1}개 있습니다.", files[i].Name, result[i]);
+    for (int i = 0; i < files.Length; i++)
+    {
+        int lineCount = 0;
+
+        foreach (string line in File.ReadLines(files[i].ToString()))
+        {
+            string Line = line.ToUpper();
+
+            int wordPos = Line.IndexOf("DROP", 0);
+            int droppingPos = Line.IndexOf("DROPPING", 0);
+
+            lineCount++;
+
+            while (wordPos != -1)
+            {
+                if (wordPos != droppingPos)
+                {
+                    Console.WriteLine("{0} : {1}줄 {2}번째 문자에 drop이 있습니다.", files[i].Name, lineCount, wordPos + 1);
+                    result[i]++;
+                }
+
+                int nextWordPos = wordPos + 1;
+
+                wordPos = Line.IndexOf("DROP", nextWordPos);
+                droppingPos = Line.IndexOf("DROPPING", nextWordPos);
+            }
+        }
+
+        Console.WriteLine("{0} 에는 drop 이 {1}개 있습니다.", files[i].Name, result[i]);
+    }
 }
