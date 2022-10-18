99b34c11489bf58b430d608504f0df7e582b431a
robo <robolab1902@naver.com>
2022-10-19 08:08:21 +0900

    Service 로 분류

--- Program.cs
+++ Program.cs
@@ -1,55 +1,17 @@
-﻿using System.Text;
+﻿using FindWord.Service;
+using System.Text;
 
-args = new string[3];
+args = new string[10];
 args[0] = "D:\\GItSource\\FindWord";
 args[1] = "drop";
 args[2] = "main";
-
-//string Dir = Environment.GetCommandLineArgs()[1];
-string Dir = args[0];
-DirectoryInfo directory = new DirectoryInfo(Dir);
-
-FileInfo[] files = directory.GetFiles();
-int[] result = new int[files.Length];
-
-List<string> GoalWords = new();
-List<string> ExceptWords = new();
+args[3] = "-except";
+args[4] = "dropping";
 
 if (args.Length > 1)
 {
-    for (int i = 1; i < args.Length; i++)
-    {
-        GoalWords.Add(args[i]);
-    }
+    FindWordService findWordService = new();
+    ClassifyService classifyService = new();
 
-    for (int i = 0; i < files.Length; i++)
-    {
-        int lineCount = 0;
-
-        foreach (string line in File.ReadLines(files[i].ToString()))
-        {
-            string Line = line.ToUpper();
-
-            int wordPos = Line.IndexOf("DROP", 0);
-            int droppingPos = Line.IndexOf("DROPPING", 0);
-
-            lineCount++;
-
-            while (wordPos != -1)
-            {
-                if (wordPos != droppingPos)
-                {
-                    Console.WriteLine("{0} : {1}줄 {2}번째 문자에 drop이 있습니다.", files[i].Name, lineCount, wordPos + 1);
-                    result[i]++;
-                }
-
-                int nextWordPos = wordPos + 1;
-
-                wordPos = Line.IndexOf("DROP", nextWordPos);
-                droppingPos = Line.IndexOf("DROPPING", nextWordPos);
-            }
-        }
-
-        Console.WriteLine("{0} 에는 drop 이 {1}개 있습니다.", files[i].Name, result[i]);
-    }
+    findWordService.FindWord(args);
 }
---
+++ Service/FindWordService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWord.Service
+{
+    public class FindWordService
+    {
+        public void FindWord(string[] args)
+        {
+            FileInfo[] files = GetFiles(args[0]);
+
+            int[] result = new int[files.Length];
+            List<string> GoalWords = new();
+            List<string> ExceptWords = new();
+
+            int exceptPos = 0;
+            // Except 위치 j
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i].Equals("-except"))
+                {
+                    exceptPos = i;
+                    break;
+                }
+                GoalWords.Add(args[i]);
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                int lineCount = 0;
+
+                foreach (string line in File.ReadLines(files[i].ToString()))
+                {
+                    string Line = line.ToUpper();
+
+                    int wordPos = Line.IndexOf("DROP", 0);
+                    int droppingPos = Line.IndexOf("DROPPING", 0);
+
+                    lineCount++;
+
+                    while (wordPos != -1)
+                    {
+                        if (wordPos != droppingPos)
+                        {
+                            Console.WriteLine("{0} : {1}줄 {2}번째 문자에 drop이 있습니다.", files[i].Name, lineCount, wordPos + 1);
+                            result[i]++;
+                        }
+
+                        int nextWordPos = wordPos + 1;
+
+                        wordPos = Line.IndexOf("DROP", nextWordPos);
+                        droppingPos = Line.IndexOf("DROPPING", nextWordPos);
+                    }
+                }
+
+                Console.WriteLine("{0} 에는 drop 이 {1}개 있습니다.", files[i].Name, result[i]);
+            }
+
+        }
+
+        public FileInfo[] GetFiles(string target)
+        {
+            string Dir = target;
+            DirectoryInfo directory = new DirectoryInfo(Dir);
+
+            return directory.GetFiles();
+        }
+    }
+}
